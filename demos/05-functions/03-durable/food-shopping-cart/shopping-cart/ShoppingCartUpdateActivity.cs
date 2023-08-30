a081f47ce724422a1595bb3a214942784eec3c43
Alexander Pajer <alexander.pajer@integrations.at>
2023-08-30 19:37:37 +0200

    WaitForExternalEvent

--- demos/05-functions/03-durable/food-shopping-cart/shopping-cart/ShoppingCartUpdateActivity.cs
+++ demos/05-functions/03-durable/food-shopping-cart/shopping-cart/ShoppingCartUpdateActivity.cs
@@ -8,22 +8,22 @@ using Microsoft.Extensions.Logging;
 
 namespace FoodApp
 {
-    public static class ShoppingCartRemoveActivity
+    public static class ShoppingCartUpdateActivity
     {
-        [FunctionName(nameof(ShoppingCartRemoveActivity))]
+        [FunctionName(nameof(ShoppingCartUpdateActivity))]
         public static async Task<HttpResponseMessage> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "cart/remove")] HttpRequestMessage req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "cart/update")] HttpRequestMessage req,
             [DurableClient] IDurableOrchestrationClient orchclient,
             ILogger logger)
         {
-            
-            var eventData = await req.Content.ReadAsAsync<OrderRemoveModel>();
-            string eventName = "RemovFromCart";
+
+            var eventData = await req.Content.ReadAsAsync<CartItem>();
+            string eventName = "UpdateCart";
             await orchclient.RaiseEventAsync(
                 eventData.OrchestrationInstanceId,
                 eventName,
                 eventData);
             return new HttpResponseMessage(HttpStatusCode.OK);
-        }    
+        }
     }
-}    
+}
