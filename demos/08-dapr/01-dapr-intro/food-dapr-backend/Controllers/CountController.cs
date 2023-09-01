9b11fb8964a0fad1f84966235ca14d9e901845b1
Alexander Pajer <alexander.pajer@integrations.at>
2023-09-01 20:59:23 +0200

    client in services

--- demos/08-dapr/01-dapr-intro/food-dapr-backend/Controllers/CountController.cs
+++ demos/08-dapr/01-dapr-intro/food-dapr-backend/Controllers/CountController.cs
@@ -14,34 +14,19 @@ namespace FoodDapr
         const string storeName = "statestore";
         const string key = "counter";
 
+        private readonly DaprClient client;
+
+        public CountController(DaprClient daprClient)
+        {
+            client = daprClient;
+        }
+
         [HttpGet("getCount")]
         public async Task<int> Get()
         {
-            var daprClient = new DaprClientBuilder().Build();
-            var counter = await daprClient.GetStateAsync<int>(storeName, key);
-            await daprClient.SaveStateAsync(storeName, key, counter + 1);
+            var counter = await client.GetStateAsync<int>(storeName, key);
+            await client.SaveStateAsync(storeName, key, counter + 1);
             return counter;
         }
-
-        // [HttpGet("{id}")]
-        // public ActionResult<string> Get(int id)
-        // {
-        //     return "value";
-        // }
-
-        // [HttpPost]
-        // public void Post([FromBody] string value)
-        // {
-        // }
-
-        // [HttpPut("{id}")]
-        // public void Put(int id, [FromBody] string value)
-        // {
-        // }
-
-        // [HttpDelete("{id}")]
-        // public void Delete(int id)
-        // {
-        // }
     }
 }
