ca396ba47e8504a12959c5ea4125aad48cc5e564
Alexander Pajer <alexander.pajer@integrations.at>
2023-09-02 19:56:02 +0200

    kv scaffold

---
+++ demos/08-dapr/02-secrets-config/kv-api-dapr/Controllers/KeyVaultController.cs
@@ -0,0 +1,35 @@
+using Azure.Core;
+using Azure.Identity;
+using Dapr.Client;
+
+// using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Mvc;
+
+namespace kv_api;
+
+[ApiController]
+[Route("[controller]")]
+public class KeyVaultController : ControllerBase
+{
+    private readonly ILogger<KeyVaultController> logger;
+    private IConfiguration cfg;
+
+    const string DAPR_SECRET_STORE = "localsecretstore";  
+    private readonly DaprClient client;
+
+    public KeyVaultController(ILogger<KeyVaultController> lgr, IConfiguration configuration, DaprClient daprClient)
+    {
+        logger = lgr;
+        cfg = configuration;
+        client = daprClient;
+    }
+
+    [HttpGet("GetValue")]
+    public async Task<string> Get()
+    {
+        var store = cfg.GetValue<string>("DAPR_SECRET_STORE");
+        var secret = await client.GetSecretAsync(store, "dapr-secret");
+        var secretValue = string.Join(", ", secret);
+        return secretValue;
+    }
+}
--- demos/08-dapr/02-secrets-config/readme.md
+++ demos/08-dapr/02-secrets-config/readme.md
@@ -1,1 +1,19 @@
 # Secrets and Configuration
+
+- Using Dapr Secrets Store
+- Using Dapr Configuration Store
+
+## Using Dapr Secrets Store
+
+Dapr’s dedicated secrets building block API makes it easier for developers to consume application secrets from a secret store. 
+
+![Dapr Secrets](_images/secrets.png)
+
+- Execute `create-kv-dapr-app.azcli` to create a Key Vault and a secret in Azure Key Vault.
+
+- Start the Dapr sidecar and the application using the following command:
+
+```bash
+cd kv-api-dapr
+dapr run --app-id dapr-kv --app-port 3500 --components-path ../components dotnet run
+```
