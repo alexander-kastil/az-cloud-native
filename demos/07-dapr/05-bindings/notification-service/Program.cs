81d9ebcece5ba2b97f479862ab5190b374a2eea3
Alexander Pajer <alexander.pajer@integrations.at>
2023-10-24 16:40:43 +0200

    binding

--- demos/07-dapr/05-bindings/notification-service/Program.cs
+++ demos/07-dapr/05-bindings/notification-service/Program.cs
@@ -40,11 +40,10 @@ app.MapPost("/" + cronBindingName, () =>
 app.MapPost("/" + paymentBindingName, async (CloudEvent<PaymentRequest> req) =>
 {
     Console.WriteLine("Received msg from Service Bus: " + req.Data);
-    var msg = $"Dear customer, your order with {req.Data.OrderId} was paid";
+    var msg = $"Dear customer, your order with id {req.Data.OrderId} was paid";
     using var client = new DaprClientBuilder().Build();
     await client.InvokeBindingAsync<string>(twilioBindingName, "create", msg);    
 })
-
 .WithName("ServiceBusTrigger")
 .WithOpenApi();
 
