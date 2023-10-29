22c8345b3b228e231f02513af7a044a39e12fa1a
Alexander Pajer <alexander.pajer@integrations.at>
2023-10-29 10:24:58 +0100

    solution parts added

---
+++ labs/lab-05/solution/order-events-processor/Infrastructure/OrdersRepository.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+
+namespace FoodApp
+{
+    public class OrdersRepository : IOrdersRepository 
+    {
+        private CosmosClient dbClient;
+        private Container container;
+        public OrdersRepository()
+        {
+            string cs = Environment.GetEnvironmentVariable("CosmosDBConnectionString");
+            string db = Environment.GetEnvironmentVariable("DBName");
+            string orders = Environment.GetEnvironmentVariable("OrdersContainer");
+            dbClient = new CosmosClient(cs);
+            container = dbClient.GetContainer(db, orders);
+        }               
+
+        public async Task<Order> GetOrderAsync(string id, string customerId)
+        {
+            try
+            {
+                ItemResponse<Order> response = await container.ReadItemAsync<Order>(id, new PartitionKey(customerId));
+                return response.Resource;
+            }
+            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+        }
+
+        public async Task CreateOrderAsync(Order order)
+        {
+            order.Status = OrderEventType.Created.ToString();
+            await container.CreateItemAsync<Order>(order, new PartitionKey(order.Customer.Id));
+        }
+
+        public async Task UpdateOrderAsync(Order order, OrderEvent orderEvent)
+        {
+            order.Events.Add(orderEvent);
+            order.Status = orderEvent.EventType;
+            await container.UpsertItemAsync<Order>(order, new PartitionKey(order.Customer.Id));
+        }
+    }
+}
