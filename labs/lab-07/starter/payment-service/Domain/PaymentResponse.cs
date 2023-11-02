93dd995eb0332d4031fead05e3d046e641016be3
Alexander Pajer <alexander.pajer@integrations.at>
2023-11-02 11:39:51 +0100

    docs

--- labs/lab-07/starter/payment-service/Domain/PaymentResponse.cs
+++ labs/lab-07/starter/payment-service/Domain/PaymentResponse.cs
@@ -3,13 +3,13 @@ using Newtonsoft.Json;
 
 namespace FoodApp
 {
-    public class PaymentRequest
+    public class PaymentResponse
     {
         [JsonProperty("id")]
         public string Id { get; set; }
         [JsonProperty("paymentInfo")]
-        public PaymentInfo PaymentInfo { get; set; }
-        [JsonProperty("amount")]
-        public decimal Amount { get; set; }
+        public string Status { get; set; }
+        [JsonProperty("data")]
+        public object Data { get; set; }
     }
 }
