f2c5b1c7ac97b4ef1788570022d9d8854a0c2505
Alexander Pajer <alexander.pajer@integrations.at>
2023-11-09 16:24:39 +0100

    deployment

--- app/web/food-shop/src/app/food/shop/order/orders.service.ts
+++ app/web/food-shop/src/app/food/shop/order/orders.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { AILoggerService } from 'src/app/logger/ai-logger.service';
-import { environment } from '../../../../environments/environment.development';
+import { environment } from '../../../../environments/environment';
 import { OrderEventResponse } from './order-event-response';
 
 @Injectable({
@@ -13,6 +13,7 @@ export class OrdersService {
 
   checkout(order: any) {
     this.logger.logEvent('ShopUI - Checkout Order', order);
+
     return this.http.post<OrderEventResponse>(`${environment.ordersApi}/orders/create`, order);
   }
 }
