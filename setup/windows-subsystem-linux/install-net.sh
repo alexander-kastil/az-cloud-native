2c73570011d942f5f4b7e3e8d61b140774df6d27
Alexander Pajer <alexander.pajer@integrations.at>
2023-11-19 10:58:49 +0100

    setup scripts updated

--- setup/windows-subsystem-linux/install-net.sh
+++ setup/windows-subsystem-linux/install-net.sh
@@ -1,3 +1,7 @@
-sudo rm /etc/apt/sources.list.d/microsoft-prod.list
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
 sudo apt update
-sudo apt install dotnet-sdk-7.0
+sudo apt install dotnet-sdk-6.0
+sudo apt install dotnet-sdk-8.0
