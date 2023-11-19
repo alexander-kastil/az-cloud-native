eee77297278c24eb64e0e12ffa38f8ae161c23e4
Alexander Pajer <alexander.pajer@integrations.at>
2023-11-19 17:50:36 +0100

    angular cloud roles (#185)

--- app/web/cooking-dashboard/src/environments/environment.ts
+++ app/web/cooking-dashboard/src/environments/environment.ts
@@ -5,5 +5,11 @@ declare global {
 }
 
 export const environment = {
-  WebhookEP: window['env']["FUNC_EP"] || 'https://cooking-dashboard-dev.azurewebsites.net',
+  title: 'Cooking Dashboard',
+  WebhookEP: 'https://cooking-dashboard-dev.azurewebsites.net',
+  loggingEnabled: true,
+  azure: {
+    apimSubscriptionKey: "39fc8b24086a4346a6317d047869f983",
+    applicationInsights: 'd1588cb9-4e43-4fdc-9aef-99436565d628'
+  }
 };
