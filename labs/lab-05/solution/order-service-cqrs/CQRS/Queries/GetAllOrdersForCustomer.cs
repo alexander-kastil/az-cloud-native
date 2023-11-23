9e8843197b8259b3d1f99a8fc96888eb8d53234b
Alexander Pajer <alexander.pajer@integrations.at>
2023-11-23 10:27:23 +0100

    lab-05 (#195)

--- labs/lab-05/solution/order-service-cqrs/CQRS/Queries/GetAllOrdersForCustomer.cs
+++
@@ -1,6 +0,0 @@
-using MediatR;
-
-namespace FoodApp
-{
-    public record GetAllOrdersForCustomer(string CustomerId) : IRequest<IEnumerable<Order>>;    
-}
---
+++ labs/lab-05/solution/order-service/CQRS/Handlers/AddOrderEventHandler.cs
@@ -0,0 +1,21 @@
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace FoodApp
+{
+    public class AddOrderEventHandler : IRequestHandler<AddOrderEventCommand, OrderEventResponse>
+    {
+        private readonly IOrderEventsStore orderEventsStore;
+
+        public AddOrderEventHandler(IOrderEventsStore eventStore)
+        {
+            orderEventsStore = eventStore;
+        }
+
+        public async Task<OrderEventResponse> Handle(AddOrderEventCommand request, CancellationToken cancellationToken)
+        {                                    
+            return await orderEventsStore.CreateOrderEventAsync(request.Event);
+        }
+    }
+}
